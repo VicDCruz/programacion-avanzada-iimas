3c8a42555f0223b2ed4d1428d4a21244b0fe104b
Victor Cruz <vicdan215@gmail.com>
2020-10-29 14:55:43 -0600

    Terminando características del ejercicio

--- tarea-3/ejercicio3/src/App.java
+++ tarea-3/ejercicio3/src/App.java
@@ -1,26 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private static int INITIAL_ACCOUNT = 10100;
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        boolean correct = false;
-        Cuenta cuentas[] = new Cuenta[10];
 
+    private static String getName(Scanner scanner) {
+        boolean correct = false;
+        String name = "";
+        
         while (!correct) {
             System.out.print("Ingresa un nombre: ");
-            String name = scanner.nextLine();
-
+            name = scanner.nextLine();
+    
             System.out.println("El sistema obtuvo el nombre: " + name);
-            System.out.print("¿Es correcto? (S/N) - " );
-            char correctStr = scanner.nextLine().charAt(0);
-            while (correctStr != 'S' && correctStr != 's' && 
-                    correctStr != 'N' && correctStr != 'n') {
+            System.out.print("¿Es correcto? (S/N) - ");
+            char correctChar = scanner.nextLine().charAt(0);
+            while (correctChar != 'S' && correctChar != 's' && correctChar != 'N' && correctChar != 'n') {
                 System.out.print("Solo introduce S/N - ");
-                correctStr = scanner.nextLine().charAt(0);
+                correctChar = scanner.nextLine().charAt(0);
             }
-            correct = correctStr == 'S' || correctStr == 's';
+            correct = correctChar == 'S' || correctChar == 's';
         }
+
+        return name;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Cuenta> cuentas = new ArrayList<Cuenta>();
+        cuentas.add(new Cuenta(getName(scanner), INITIAL_ACCOUNT + cuentas.size() + "", 500));
+        cuentas.add(new Cuenta(getName(scanner), INITIAL_ACCOUNT + cuentas.size() + ""));
+        System.out.println("--------------");
+        for (Cuenta cuenta : cuentas) {
+            System.out.println(cuenta.toString());
+            System.out.println("--------------");
+        }
+        cuentas.get(0).transferirDinero(cuentas.get(1), 200);
+        System.out.println("--------------");
+        System.out.println("Saldos actualizados");
+        for (Cuenta cuenta : cuentas) {
+            System.out.println(cuenta.toString());
+            System.out.println("--------------");
+        }
+        System.out.println("Ingresando $50 a la cuenta de " + cuentas.get(0).getOwner());
+        cuentas.get(0).ingresarSaldo(50);
+        System.out.println("--------------");
+        System.out.println("Descontando $170 de la cuenta de " + cuentas.get(1).getOwner());
+        cuentas.get(1).descontarSaldo(170);
+        System.out.println("--------------");
+        System.out.println("Creando 3 nuevas cuentas");
+        cuentas.add(new Cuenta("Juan", INITIAL_ACCOUNT + cuentas.size() + ""));
+        cuentas.add(new Cuenta("Tere", INITIAL_ACCOUNT + cuentas.size() + ""));
+        cuentas.add(new Cuenta("David", INITIAL_ACCOUNT + cuentas.size() + ""));
+        System.out.println("Ahora hay " + cuentas.size() + "cuentas, en total");
+        for (Cuenta cuenta : cuentas) {
+            System.out.println(cuenta.toString());
+            System.out.println("--------------");
+        }
+
         scanner.close();
     }
 }
--- tarea-3/ejercicio3/src/Cuenta.java
+++ tarea-3/ejercicio3/src/Cuenta.java
@@ -4,44 +4,122 @@
 public class Cuenta {
 
     private String owner;
-    // No haremos operaciones matemáticas, 
+    // No haremos operaciones matemáticas,
     // solo es una cadena representativa
     private String accountNumber;
     private double money;
 
+    /**
+     * Cuenta
+     * 
+     * @param owner
+     * @param accountNumber
+     */
     public Cuenta(String owner, String accountNumber) {
         this.owner = owner;
         this.accountNumber = accountNumber;
         this.money = 0;
     }
 
+    /**
+     * Cuenta
+     * 
+     * @param owner
+     * @param accountNumber
+     * @param money
+     */
     public Cuenta(String owner, String accountNumber, double money) {
         this.owner = owner;
         this.accountNumber = accountNumber;
         this.money = money;
     }
 
+    /**
+     * 
+     * @return
+     */
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    /**
+     * 
+     * @param accountNumber
+     */
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
+    /**
+     * 
+     * @return
+     */
     public double getMoney() {
         return money;
     }
 
+    /**
+     * 
+     * @param money
+     */
     public void setMoney(double money) {
         this.money = money;
     }
 
+    /**
+     * 
+     * @return
+     */
     public String getOwner() {
         return owner;
     }
 
+    /**
+     * 
+     * @param owner
+     */
     public void setOwner(String owner) {
         this.owner = owner;
+    }
+
+    /**
+     * 
+     * @param money
+     */
+    public void ingresarSaldo(double money) {
+        this.money += money;
+    }
+
+    /**
+     * 
+     * @param money
+     */
+    public void descontarSaldo(double money) {
+        if (this.money >= money)
+            this.money -= money;
+        else
+            System.out.println("Fondos insuficientes");
+    }
+
+    /**
+     * 
+     * @param other
+     * @param transfer
+     */
+    public void transferirDinero(Cuenta other, double transfer) {
+        System.out.println("Verificando datos...");
+        System.out.println("Transfiriendo dinero de la cuenta de " + this.owner + " a " + other.getOwner());
+        if (this.money >= transfer) {
+            this.money -= transfer;
+            other.ingresarSaldo(transfer);
+            System.out.println("Dinero transferido! " + this.owner + ", tu nuevo saldo es de $" + this.money);
+        } else
+            System.out.println("No hay dinero suficiente para transferir a esta cuenta");
+    }
+
+    @Override
+    public String toString() {
+        return "|Propietario: " + this.owner + "\t" + "Número de cuenta: " + this.accountNumber + "\t\t" + "Dinero: $"
+                + this.money + "|";
     }
 }
